d0e023bf3f328c094f87d77fdf893cac0df187b5
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2022-06-10 09:12:02 -0400

    feat: add repro

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+import { ExploreContainerComponent } from './explore-container/explore-container.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ExploreContainerComponent
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
